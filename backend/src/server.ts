947724c7075734173ffff65025e9f1243128634a
TheBlackKnight9 <codedemon27@gmail.com>
2026-05-27 16:36:39 +0530

    feat: initialize Express server in src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ app.use(
     origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept", "Cache-Control", "Pragma", "Expires"],
   })
 );
 
